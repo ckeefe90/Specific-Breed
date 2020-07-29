79d99e4419113eb501ee2c3126745af12a100eb1
Caitlin Keefe <caitlinkeefe@thinkful.com>
2020-07-28 22:31:17 -0400

    search by breed

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+function getDogImage() {
+    const breed = $('#breed').val();
+    fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+        .then(response => {
+            if (response.ok)
+                return response.json();
+            throw "Unable to locate this breed, please select a new breed."
+        })
+        .then(responseJson => {
+            console.log(responseJson);
+            displayResults(responseJson);
+        })
+        .catch(error => alert(error));
+}
+
+function displayResults(responseJson) {
+    $('.results').append(
+        `<img src="${responseJson.message}">`
+    )
+}
+
+function watchForm() {
+    $('form').submit(event => {
+        event.preventDefault();
+        getDogImage();
+    });
+}
+
+$(function() {
+    watchForm();
+});
